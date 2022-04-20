e2afafbd9cfa493357c459997fddb7b31cdcf3b2
CheeseburgerIM <13051172893@163.com>
2022-04-20 22:16:28 +0800

    2022/04/20

---
+++ FinalAssignment/FrontEnd/demo/src/components/CodeTest.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="CodeTest">
+    <editor
+      v-model="content"
+      @init="editorInit"
+      lang="html"
+      theme="chrome"
+      width="90%"
+      height="100%"
+    ></editor>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CodeTest",
+  components: {
+    editor: require("vue2-ace-editor"),
+  },
+  methods: {
+    editorInit: function () {
+      require("brace/ext/language_tools"); //language extension prerequsite...
+      require("brace/mode/html");
+      require("brace/mode/javascript"); //language
+      require("brace/mode/less");
+      require("brace/theme/chrome");
+      require("brace/snippets/javascript"); //snippet
+    },
+  },
+  data() {
+    return {
+      content: "print('Hello World!')",
+    };
+  },
+};
+</script>
+
+<style>
+#CodeTest {
+  margin-top: 27px;
+  height: 70vh;
+  width: 50%;
+}
+</style>
---
+++ FinalAssignment/FrontEnd/demo/src/components/IO.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="IO">
+    <el-input
+      type="textarea"
+      :autosize="{ minRows: 7, maxRows: 12 }"
+      placeholder="请输入传入参数"
+      v-model="text"
+      size="large"
+    >
+    </el-input>
+    <div style="margin: 17px 0"></div>
+    <el-button type="primary" plain>代码测试 RUN</el-button>
+    <div style="margin: 17px 0"></div>
+    <el-input
+      type="textarea"
+      :autosize="{ minRows: 12, maxRows: 17 }"
+      placeholder="请输入传入参数"
+      v-model="text"
+      size="large"
+      disabled="true"
+    >
+    </el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IO",
+  data() {
+    return {
+      text: "",
+    };
+  },
+};
+</script>
+
+<style>
+#IO {
+  width: 50%;
+  margin-top: 27px;
+}
+</style>
